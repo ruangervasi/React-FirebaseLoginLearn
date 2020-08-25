fb9c245a4fa3280b0a74312b47d8d7cdd285967c
Ruan Marcos Gervasi <ruan@ruangervasi.com.br>
2020-08-25 15:49:35 -0300

    Estilizacao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import firebase from './fireConnection';
+import { TextField, Container, AppBar, Toolbar, IconButton, MenuIcon, Typography, Button, CardContent, Card } from '@material-ui/core';
+// import { MenuIcon } from '@material-ui/icons';
 
 class App extends Component {
     constructor(props) {
@@ -66,38 +68,47 @@ class App extends Component {
     render() {
         return (
             <div>
-                {this.state.logar === 1 ? 
-                <div>
-                    <form onSubmit={this.cadastrar}>
-                    <h1>Entrar</h1>
-                    <label>E-mail: </label><br/>
-                    <input type="text" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/><br/>
-                    <label>Senha: </label><br/>
-                    <input type="text" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/><br/>
-                    <button type="submit">
-                        Entrar
-                    </button>
-                    </form>
-                    <button onClick={(e) => { this.setState({ logar: 0 }); this.sair()}}>Não possui cadastro? Cadastre-se</button>
-                </div>                
-                :
-                <div>
-                    <form onSubmit={this.cadastrar}>
-                        <h1>Cadastrar</h1>
-                        <label>Nome: </label><br/>
-                        <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/><br/>
-                        <label>E-mail: </label><br/>
-                        <input type="text" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/><br/>
-                        <label>Senha: </label><br/>
-                        <input type="text" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/><br/>
-                        <button type="submit">
-                            Cadastrar
-                        </button>
-                    </form> 
-                    <button onClick={(e) => { this.setState({ logar: 1 })}}>Já tem conta? Fazer login</button>
-                </div>
-                }
-            {this.state.logado === 1 ? <button onClick={this.sair}>Sair</button>:null }
+                <AppBar position="static">
+                    <Toolbar>
+                        {/* <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                        </IconButton> */}
+                        <Typography variant="h6" className="menuTitle">
+                        Tela de cadastro
+                        </Typography>
+                        <Button color="inherit"></Button>
+                    </Toolbar>
+                </AppBar>
+
+                <Container maxWidth="sm">
+                    {this.state.logar === 1 ? 
+                    <div>
+                        <form onSubmit={this.cadastrar}>
+                        <h1>Entrar</h1>
+                        <TextField id="outlined-basic" label="E-mail" type="text" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/><br/>
+                        <TextField id="outlined-basic" label="Senha" type="text" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/><br/><br/>
+                        <Button variant="contained" color="primary" disableElevation type="submit">
+                            Entrar
+                        </Button><br/><br/>
+                        </form>
+                        <Button variant="contained" color="grey" disableElevation onClick={(e) => { this.setState({ logar: 0 }); this.sair()}}>Não possui cadastro? Cadastre-se</Button>
+                    </div>                
+                    :
+                    <div>
+                        <form onSubmit={this.cadastrar}>
+                            <h1>Cadastrar</h1>
+                            <TextField id="outlined-basic" label="Nome" type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/><br/>
+                            <TextField id="outlined-basic" label="E-mail" type="text" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/><br/>
+                            <TextField id="outlined-basic" label="Senha" type="text" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/><br/><br/>
+                            <Button variant="contained" color="primary" disableElevation type="submit">
+                                Cadastrar
+                            </Button><br/><br/>
+                        </form> 
+                        <Button variant="contained" color="grey" disableElevation onClick={(e) => { this.setState({ logar: 1 })}}>Já tem conta? Fazer login</Button>
+                    </div>
+                    }
+                {this.state.logado === 1 ? <Button variant="contained" color="primary" disableElevation onClick={this.sair}>Sair</Button>:null }
+            </Container>
             </div>
         );
     }
