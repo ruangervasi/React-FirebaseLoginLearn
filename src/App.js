a6b6b45dcbb5512dd787f2e772c61e90734fde4c
Ruan Marcos Gervasi <ruan@ruangervasi.com.br>
2020-08-25 15:53:32 -0300

    CorrecaoEstilizacao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import firebase from './fireConnection';
-import { TextField, Container, AppBar, Toolbar, IconButton, MenuIcon, Typography, Button, CardContent, Card } from '@material-ui/core';
-// import { MenuIcon } from '@material-ui/icons';
+import { TextField, Container, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 class App extends Component {
     constructor(props) {
@@ -70,9 +69,6 @@ class App extends Component {
             <div>
                 <AppBar position="static">
                     <Toolbar>
-                        {/* <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                        </IconButton> */}
                         <Typography variant="h6" className="menuTitle">
                         Tela de cadastro
                         </Typography>
